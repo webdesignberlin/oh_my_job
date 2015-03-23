0b4a7ddecb79db7c5491fff946b8833f79f13909
Benoit <benoit@hopsandfork.com>
2015-03-23 12:01:47 +0100

    Trying to fix mongo import with ruby script

--- json_postgres_import.rb
+++ json_postgres_import.rb
@@ -1,16 +1,22 @@
 require 'json'
 
 file = File.open("./dataset.json", "r")
-jobs_json = JSON.parse(file.read)[0]["JobData"]
+jobs_json = JSON.parse(file.read)
 file.close
 
-jobs_json.each do |job_json|
-  job_json_first = {}
-  job_json.each do |key, value|
-    job_json_first[key.underscore] = value
+jobs_json.each do |jobs_json_data|
+  jobs_json_data.each do |job_json_data|
+    job_json_first = {}
+    next unless job_json_data[0]=="JobData"
+    job_json_data[1].each do |data|
+      data.each do |key, value|
+        puts key.inspect
+        job_json_first[key.underscore] = value
+      end
+    end
+    JobOffer.create(job_json_first)
+    puts "#{JobOffer.count} : #{JobOffer.last.inspect}"
   end
-  JobOffer.create(job_json_first)
-  puts JobOffer.last.inspect
 end
 
 
